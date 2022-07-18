c2569ec0e65282d44d01cb894dcac340bc407afa
Luciana <104689017+lusz24@users.noreply.github.com>
2022-07-18 10:11:23 -0300

    Add files via upload

---
+++ primeraparte.js
@@ -0,0 +1,14 @@
+//Crear una función con tres parámetros que sean números que se suman entre sí.
+//Llamar a la función en el main y darle valores.
+
+let param1 = 2;
+let param2 = 3;
+let param3 = 4;
+
+function suma() {
+    let resultado = param1 + param2 + param3;
+    console.log(resultado);
+    return resultado;
+}
+
+suma();
